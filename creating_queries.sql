1af32839cf59badad6f41385d953a68191cd4a4a
ahmedsalah-iti <ahmedsalah.iti@gmail.com>
2025-02-07 17:37:40 +0200

    fixing some syntax errors

--- creating_queries.sql
+++ creating_queries.sql
@@ -44,7 +44,7 @@ CREATE TABLE User(
 CREATE TABLE Wallet_Transaction(
     id int AUTO_INCREMENT primary key,
     user_id int not null,
-    type ENUM(0,1) not null, -- 0 add ,  1 sub
+    type ENUM('add','sub') not null,
     amount decimal(10,2) not null,
     balance_before decimal(10,2) not null,
     balance_after decimal(10,2) not null,
@@ -58,15 +58,17 @@ CREATE table Product (
     category_id int not null,
     status BOOLEAN DEFAULT FALSE not null
 );
-CREATE TABLE Order (
+
+CREATE table `Order` (
     id int AUTO_INCREMENT primary key,
     user_id int not null,
     status ENUM('pending', 'completed', 'canceled') DEFAULT 'pending' not null,
     total_price decimal(10,2) not null,
     note VARCHAR(255),
     room_id int not null,
-    created_at DATETIME CURRENT_TIMESTAMP not null
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP not null
 );
+
 CREATE TABLE Order_Product(
     id int AUTO_INCREMENT primary key,
     order_id int not null,
@@ -74,11 +76,12 @@ CREATE TABLE Order_Product(
     quantity tinyint not null,
     price decimal(10,2) not null
 );
+
 CREATE TABLE Payment(
     id int AUTO_INCREMENT PRIMARY KEY,
     order_id int not null,
     amount decimal(10,2) not null,
     method ENUM('wallet','card','delivery') not null,
     status ENUM('pending','completed','failed') DEFAULT 'pending' not null,
-    made_at DATETIME CURRENT_TIMESTAMP  not null
+    made_at DATETIME DEFAULT CURRENT_TIMESTAMP not null
 );
