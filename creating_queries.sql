39d98cc6dd79a814b0485071e634ec0b26dc3ee8
ahmedsalah-iti <ahmedsalah.iti@gmail.com>
2025-02-07 18:49:20 +0200

    added the left foreign keys and fixed some syntax issues.

--- creating_queries.sql
+++ creating_queries.sql
@@ -37,7 +37,7 @@ CREATE TABLE User(
     password VARCHAR(255) not null,
     room_id int,
     profile_pic_id int ,
-    role_id int not null,
+    role_id int,
     wallet_balance decimal(10,2) DEFAULT 0.00  not null,
     joined_at DATETIME DEFAULT CURRENT_TIMESTAMP not null
 );
@@ -61,54 +61,83 @@ CREATE table Product (
 
 CREATE table `Order` (
     id int AUTO_INCREMENT primary key,
-    user_id int not null,
+    user_id int,
     status ENUM('pending', 'completed', 'canceled') DEFAULT 'pending' not null,
     total_price decimal(10,2) not null,
     note VARCHAR(255),
-    room_id int not null,
+    room_id int,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP not null
 );
 
 CREATE TABLE Order_Product(
     id int AUTO_INCREMENT primary key,
     order_id int not null,
-    product_id int not null,
+    product_id int,
     quantity tinyint not null,
     price decimal(10,2) not null
 );
 
 CREATE TABLE Payment(
     id int AUTO_INCREMENT PRIMARY KEY,
-    order_id int not null,
+    order_id int,
     amount decimal(10,2) not null,
     method ENUM('wallet','card','delivery') not null,
     status ENUM('pending','completed','failed') DEFAULT 'pending' not null,
     made_at DATETIME DEFAULT CURRENT_TIMESTAMP not null
 );
 
-------------- Adding The Foreign Keys -------------
-
+-- Adding The Foreign Keys
 -- user's fks
 ALTER TABLE User add CONSTRAINT fk_user_room_id
-FOREIGN KEY (room_id) REFERENCES room(id)
+FOREIGN KEY (room_id) REFERENCES Room(id)
 on DELETE set null;
 
 ALTER TABLE User add CONSTRAINT fk_user_profile_pic_id
-FOREIGN key (profile_pic_id) REFERENCES profiel_pic(id);
+FOREIGN key (profile_pic_id) REFERENCES Profile_Pic(id)
 on DELETE set null;
 
 ALTER TABLE User add CONSTRAINT fk_user_role_id
-FOREIGN KEY (role_id) REFERENCES role(id)
-on DELETE set 0;
+FOREIGN KEY (role_id) REFERENCES Role(id)
+on DELETE set null;
 
 -- profile_pic's fks
 ALTER TABLE Profile_Pic add CONSTRAINT fk_profile_pic_user_id
-FOREIGN KEY (user_id) REFERENCES user(id)
+FOREIGN KEY (user_id) REFERENCES User(id)
 on DELETE CASCADE;
 
 -- wallet_transaction's fks
 alter TABLE Wallet_Transaction add CONSTRAINT fk_wallet_transaction_user_id
-FOREIGN KEY (user_id) REFERENCES user(id)
+FOREIGN KEY (user_id) REFERENCES User(id)
 on DELETE CASCADE;
 
 -- order's fks
+alter table `Order` add CONSTRAINT fk_order_user_id
+FOREIGN key (user_id) REFERENCES User(id)
+on DELETE set null;
+
+ALTER TABLE `Order` add CONSTRAINT fk_order_room_id
+FOREIGN key (room_id) REFERENCES Room(id)
+on DELETE set null;
+
+-- product's fks
+ALTER TABLE Product add CONSTRAINT fk_product_product_pic_id
+FOREIGN KEY (product_pic_id) REFERENCES Product_Pic(id)
+on DELETE set null;
+
+ALTER TABLE Product add CONSTRAINT fk_product_category_id
+FOREIGN key (category_id) REFERENCES Category(id)
+on DELETE CASCADE;
+
+-- order_product's fks
+ALTER table Order_Product add CONSTRAINT fk_order_product_order_id
+FOREIGN KEY (order_id) REFERENCES `Order`(id)
+on DELETE CASCADE;
+
+ALTER TABLE Order_Product add CONSTRAINT fk_order_product_product_id
+FOREIGN KEY (product_id) REFERENCES Product(id)
+on DELETE set null;
+
+-- payment's fks
+ALTER table Payment add CONSTRAINT fk_payment_order_id
+FOREIGN KEY (order_id) REFERENCES `Order`(id)
+on DELETE set null;
